668354414a65b43b7b374764d51b621b9bad34a3
higgeti <higgeti@icloud.com>
2020-08-28 12:35:05 -0300

    Add files via upload

---
+++ lumberjackHACKnSLASH_working.ino
@@ -0,0 +1,45 @@
+#include <Keyboard.h>
+char left = KEY_LEFT_ARROW;
+char right = KEY_RIGHT_ARROW;
+int analogPinl = A1;
+int analogPinr = A0;
+int vall = 0;
+int valr = 0;
+void setup() {
+
+  
+ Keyboard.begin();
+}
+
+void loop() {
+     Keyboard.releaseAll();
+  while (digitalRead(2) == HIGH) {
+    // do nothing until pin 2 goes low
+    delay(500);
+  }
+
+  vall = analogRead(analogPinl);
+  delay(10);
+  valr = analogRead(analogPinr);
+  delay(10);
+  // new document:
+  if(vall > 600){
+  Keyboard.press(left);
+   Keyboard.releaseAll();
+   delay(30);
+   Keyboard.press(left);
+   Keyboard.releaseAll();
+  delay(100);
+  }
+  if(valr > 400){
+      Keyboard.press(right);
+   Keyboard.releaseAll();
+   delay(30);
+   Keyboard.press(right);
+   Keyboard.releaseAll();
+  delay(100);
+  }
+  
+  delay(140);
+
+}
